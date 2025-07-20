2303d46f433c8f1be5a5b9af566efbe566cd6341
Abner Andrade <abnerandrade@USERnoMacBook-Pro.local>
2025-07-20 01:30:07 -0300

    first commit

---
+++ src/main/java/com/application/simpleorderingsystem/SimpleOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.application.simpleorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/simpleorderingsystem/SimpleOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.simpleorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
