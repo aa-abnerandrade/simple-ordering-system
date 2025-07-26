ff604b866a58d0024be0df776e82da291c5dfdfb
Abner Andrade <abnerandrade@USERnoMacBook-Pro.local>
2025-07-25 21:13:43 -0300

    chore(repository): add user repository

--- src/main/java/com/application/simpleorderingsystem/repositories/UserRepository.java
+++ src/main/java/com/application/simpleorderingsystem/repositories/UserRepository.java
@@ -1,4 +1,7 @@
-package com.application.simpleorderingsystem.repository;
+package com.application.simpleorderingsystem.repositories;
 
-public class UserRepository {
+import com.application.simpleorderingsystem.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
