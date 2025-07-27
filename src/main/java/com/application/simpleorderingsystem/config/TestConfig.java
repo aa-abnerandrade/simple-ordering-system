34938779b39933ad87fe549e4b42045e5b5855e9
Abner Andrade <abnerandrade@USERnoMacBook-Pro.local>
2025-07-27 18:59:44 -0300

    chore(order): add order enum

--- src/main/java/com/application/simpleorderingsystem/config/TestConfig.java
+++ src/main/java/com/application/simpleorderingsystem/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.application.simpleorderingsystem.config;
 
 import com.application.simpleorderingsystem.entities.Order;
 import com.application.simpleorderingsystem.entities.User;
+import com.application.simpleorderingsystem.entities.enums.OrderStatus;
 import com.application.simpleorderingsystem.repositories.OrderRepository;
 import com.application.simpleorderingsystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Maria Brown", "maria@email.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@email.com", "977777777", "123456");
-        Order order1 = new Order(null, Instant.parse("2025-01-01T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2025-02-01T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2025-03-01T09:42:10Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2025-01-01T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+        Order order2 = new Order(null, Instant.parse("2025-02-01T03:42:10Z"), OrderStatus.SHIPPED, user2);
+        Order order3 = new Order(null, Instant.parse("2025-03-01T09:42:10Z"), OrderStatus.CANCELED, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
